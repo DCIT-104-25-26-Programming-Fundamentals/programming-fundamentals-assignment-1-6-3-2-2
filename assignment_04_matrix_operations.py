1229987e857f531e3f3227772a49a7dff8b4a891
Michelle Borlor-Boatrie <borlormichelle@gmail.com>
2026-07-28 19:33:43 +0000

    Matrix operations done

--- assignment_04_matrix_operations.py
+++ assignment_04_matrix_operations.py
@@ -60,3 +60,91 @@
 # YOUR CODE BELOW — remove the # symbols from the scaffold and fill it in
 # =============================================================================
 
+def read_matrix():
+    matrix = []
+    try:
+        row_size = int(input("Enter number of rows: "))
+        col_size = int(input("Enter number of columns: "))
+        if (row_size or col_size) <= 0:
+            print("Error: Please enter a positive integer.")
+            
+    except ValueError:
+        print("Error: Invalid input")
+        
+    for i in range(1, row_size + 1):
+        row = (input(f"Enter row {i}: ")).split(" ")
+        row = [int(i) for i in row]
+        matrix.append(row)
+    
+    return matrix
+    
+    
+def write_matrix(matrix):
+    for row in matrix:
+        row = [str(x) for x in row]
+        row = ' '.join(row)
+        print(row)
+        
+    return
+        
+def transpose(matrix):
+    
+    final_matrix = []
+    for col in range(len(matrix[0])):
+        newRow =[]
+        for row in range(len(matrix)):
+            newRow.append((matrix[row])[col])
+        final_matrix.append(newRow)
+            
+    return final_matrix
+
+def add_matrices(matrix1, matrix2):
+    if len(matrix1) != len(matrix2) or len(matrix1[0]) != len(matrix2[0]):
+        print("These matrices cannot be added because they have different sizes.")
+        return
+    final_matrix = []
+    for row in range(len(matrix1)):
+        newRow = []
+        for col in range(len(matrix1[0])):
+             element = matrix1[row][col]+matrix2[row][col]
+             newRow.append(element)
+        final_matrix.append(newRow)
+    
+    print("\n Sum: \n")   
+    write_matrix(final_matrix)
+    return
+            
+def multiply_matrices(matrix1,matrix2):
+    if len(matrix1[0]) != len(matrix2):
+        print("These matrices cannot be multiplied because the number of columns of the first are not the same as the number of rows of the second.")
+        return
+    matrix2 = transpose(matrix2)
+    final_matrix = []
+    for row1 in range(len(matrix1)):
+        newRow=[]
+        for row2 in range(len(matrix2)):
+            element = 0
+            for col in range(len(matrix1[0])):
+                element+=(matrix1[row1][col] * matrix2[row2][col])
+            newRow.append(element)
+        final_matrix.append(newRow)
+    write_matrix(final_matrix)
+    return 
+
+print(f"Transposition of Matrices\n{'-' * 30}\n")
+matrix = read_matrix()
+print("\nTransposed Matrix: ")
+write_matrix(transpose(matrix))
+    
+    
+print(f"\nAddition of matrices\n{'-' * 30}\nFor matrix A:\n{'-' * 30}")
+myMatrix1 = read_matrix()
+print(f"\n{'-' * 30}\nFor matrix B:\n{'-' * 30}")
+myMatrix2 = read_matrix()
+add_matrices(myMatrix1,myMatrix2)
+
+print(f"\nMultiplication of matrices\n{'-' * 30}\nFor matrix A:\n{'-' * 30}")
+myMatrix1 = read_matrix()
+print(f"\n{'-' * 30}\nFor matrix B:\n{'-' * 30}")
+myMatrix2 = read_matrix()
+multiply_matrices(myMatrix1,myMatrix2)
