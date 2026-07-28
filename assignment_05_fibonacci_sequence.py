87df598e8137507ac4101871486de04b6df6f27f
Michelle Borlor-Boatrie <borlormichelle@gmail.com>
2026-07-28 00:29:10 +0000

    Fibonacci sequence done

--- assignment_05_fibonacci_sequence.py
+++ assignment_05_fibonacci_sequence.py
@@ -49,3 +49,41 @@
 # YOUR CODE BELOW — remove the # symbols from the scaffold and fill it in
 # =============================================================================
 
+def fibonacci(n):
+    sequence = [0, 1]
+    if n==0:
+        return []
+    elif n == 1:
+        return [0]
+    elif n < 0:
+        return "Error: The number of terms cannot be negative."
+    else:
+        for i in range(2):
+            while len(sequence) <= (n-1):
+                nextVal = sequence[i-1] + sequence[i-2]
+                sequence.append(nextVal)
+    
+    sequence = [str(i) for i in sequence]            
+    result = "Fibonacci sequence: " + ' '.join(sequence)
+    return result
+    
+n = int(input("How many terms? "))
+print(fibonacci(n))
+
+
+def isInFibonacci(number):
+    sequence = [0, 1]
+    for i in range(2):
+        while sequence[-1]<= (number):
+            nextVal = sequence[i-1] + sequence[i-2]
+            sequence.append(nextVal)
+                
+    if number in sequence:
+        result = f"{number} is a fibonacci number."
+    else:
+        result = f"{number} is NOT a fibonacci number."
+        
+    return result
+    
+number = int(input("Enter a number to check: "))
+print(isInFibonacci(number))
