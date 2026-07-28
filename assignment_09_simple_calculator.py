d0f26f994d7f343cc12d2d8628c46aba4be30e0b
Michelle Borlor-Boatrie <borlormichelle@gmail.com>
2026-07-28 14:15:26 +0000

    Simple calculator done

--- assignment_09_simple_calculator.py
+++ assignment_09_simple_calculator.py
@@ -68,3 +68,95 @@
 # YOUR CODE BELOW — remove the # symbols from the scaffold and fill it in
 # =============================================================================
 
+
+def add(num1,num2):
+    result =num1 + num2
+    print(f"Result: {num1} + {num2} = {result}")
+    return
+    
+def subtract(num1,num2):
+    result =num1 - num2
+    print(f"Result: {num1} - {num2} = {result}")
+    return
+    
+    
+def multiply(num1,num2):
+    result =num1 * num2
+    print(f"Result: {num1} * {num2} = {result}")
+    return
+    
+def divide(dividend,divisor):
+    result = round(dividend / divisor, 2)
+    print(f"Result: {dividend} / {divisor} = {result}")
+    return
+    
+    
+def modulus(num1,modulus):
+    result = num1 % modulus
+    print(f"Result: {num1} % {modulus} = {result}")
+    return
+    
+def exponent(base,power):
+    result = base ** power
+    print(f"Result: {base} ** {power} = {result}")
+    return
+    
+def quit():
+    print('Goodbye!')
+    return
+    
+    
+print(f"""============================
+     SIMPLE CALCULATOR
+============================
+   1. Addition
+   2. Subtraction
+   3. Multiplication
+   4. Division
+   5. Modulus
+   6. Exponentiation
+   7. Quit""")
+
+while True:
+    try:
+        operation = int(input("\nSelect an operation (1-7): "))
+        if not(1<=operation<=7):
+            print("Error: Invalid input.")
+            continue
+        elif operation == 7:
+            quit()
+            break
+            
+        num1 = float(input("Enter the first number: "))
+        num2 = float(input("Enter the second number: "))
+        if str(num1).endswith('.0'):
+            num1 = int(num1)
+            
+        if str(num2).endswith('.0'):
+            num2 = int(num2)
+
+    except ValueError:
+        print("Error: Invalid input.")
+        continue
+        
+    if operation == 1:
+        add(num1,num2)
+    elif operation == 2:
+        subtract(num1,num2)
+    elif operation == 3:
+        multiply(num1,num2)
+    elif operation == 4:
+        if num2 == 0:
+            print("Division by zero is not possible.")
+            continue
+        divide(num1,num2)
+    elif operation == 5:
+        if not(str(int(num2)).isdigit()):
+            print("Error:Invalid input")
+            continue
+        elif num2 == 0:
+            print("Division by zero is not possible.")
+            continue
+        modulus(num1,int(num2))
+    elif operation == 6:
+        exponent(num1,num2)
